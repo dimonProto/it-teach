3f3b4e7372559175c190ef82a9e495fe375908d9
dimonProto <dimonvkedax@gmail.com>
2021-08-04 11:57:46 +0300

    space

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import React from 'react';
 import './App.css';
-import './Header';
-import './Technologies';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Nav from './components/Navbar/Navbar';
+
 
 const App = () => {
   return (
-    <div>
-   
-      <Header/>
-      <Technologies/>
+    <div className="app-wrapper">
+        <Header/>
+        <Nav/>
+        <Profile/>
+
     </div>
 
   );
